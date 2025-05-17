b6fe7913d047a645dadfbe00d77ca29753962fd1
Vladislav Atoyan <atoyan.1913@mail.ru>
2025-05-17 14:47:17 +0300

    -

--- diplom/diplom/settings.py
+++ diplom/diplom/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-59d@!@pjm+389f5!z3d%**h1l5_6d=2pgyid!#3r1!z)@&8kks'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
